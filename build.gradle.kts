22665e01c7d40e075969d47f7112962ee525c2f1
Dan Chao <dan.chao@apple.com>
2025-12-09 16:12:18 -0800

    Fix tests, update CI to use newly introduced lockfiles

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,6 +99,7 @@ pkl {
         projectDirectories.from(projectDirs)
         outputPath.set(outputDir.dir("generated/packages/%{name}/%{version}"))
         junitReportsDir.set(outputDir.dir("test-results"))
+        externalProperties.put("mode", "test")
         color = true
       }
     }
