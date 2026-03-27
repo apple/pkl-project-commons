74fd3809605470c6b6d7b8b7b12dfa30cfa39d88
Jen Basch <jbasch94@gmail.com>
2026-03-27 10:03:14 -0700

    Add a script for upgrading Gradle across all Pkl repos (#74)

--- scripts/repos.sh
+++ scripts/repos.sh
@@ -1,3 +1,5 @@
+set -eo pipefail
+
 export REPOS=(
   "pkl"
   "pkl-go"
@@ -21,3 +23,28 @@ export REPOS=(
   "highlightjs-pkl"
   "pkl-readers"
 )
+
+function repo_dir() {
+  echo "$SCRIPT_DIR/../build/update_downstream_ci/$1"
+}
+
+function fetch_repo() {
+  if ! GH_PAGER='' gh repo view "$MY_GIT_USER/$1" --json name > /dev/null 2> /dev/null; then
+    echo "🍽️ Forking $1..."
+    gh repo fork --default-branch-only --clone=false "apple/$1"
+  fi
+  echo "📦 Fetching $1..."
+  REPO_DIR="$(repo_dir "$1")"
+  if [[ ! -d "$REPO_DIR" ]]; then
+    git clone -o upstream "git@github.com:apple/$1.git" "$REPO_DIR" 2>&1 | grep -v "Cloning into" || true
+    cd "$REPO_DIR"
+    git remote add origin "git@github.com:$MY_GIT_USER/$1.git" 2>&1
+  else
+    cd "$REPO_DIR"
+    git fetch upstream 2>&1 | grep -v "From github.com" || true
+    git reset &> /dev/null
+    git checkout . &> /dev/null
+    git checkout main &> /dev/null
+    git reset --hard upstream/main &> /dev/null
+  fi
+}
